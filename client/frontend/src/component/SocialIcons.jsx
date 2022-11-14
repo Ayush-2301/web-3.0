609739d782cb79ed6fa81cf19149f60a104215e1
rishu15 <hrishikdwivedi@gmail.com>
2022-11-14 23:48:11 +0530

    some optimisation

--- client/frontend/src/component/SocialIcons.jsx
+++ client/frontend/src/component/SocialIcons.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const SocialIcons = ({Icons}) => {
   return (
-    <div className='text-teal-500'>
+    <div className='text-teal-500 mr-10'>
         {Icons.map(icon=>
             <span key={icon.name} className='p-2 cursor-pointer inline-flex 
-             items-center justify-center rounded-full bg-gray-700 mx-1.5 text-xl 
+             items-center justify-center self-end rounded-full bg-gray-700 mx-1.5 text-xl 
              hover:text-gray-100 hover:bg-teal-500 duration-300'>
                 <ion-icon name={icon.name}></ion-icon>
             </span>
